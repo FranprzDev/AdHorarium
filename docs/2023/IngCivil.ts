15a56a2cb580307316e87b45270278d6ab078b12
Francisco Perez <francisco.perez@rollingcodestudio.com>
2025-06-11 00:15:47 -0300

    add docs to careers

---
+++ docs/2023/IngCivil.ts
@@ -0,0 +1,284 @@
+import { RegimenCorrelatividades } from "../interfaces/regimen";
+
+export const plan_ing_civil_2023: RegimenCorrelatividades = {
+    "carrera": "Ingeniería Civil",
+    "plan": "2023",
+      "regimen_correlatividades": [
+        {
+          "nivel": "I",
+          "asignaturas": [
+            {
+              "numero": 1,
+              "nombre": "ANÁLISIS MATEMÁTICO I",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": []
+            },
+            {
+              "numero": 2,
+              "nombre": "ÁLGEBRA Y GEOMETRÍA ANALÍTICA",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": []
+            },
+            {
+              "numero": 3,
+              "nombre": "INGENIERÍA Y SOCIEDAD",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": []
+            },
+            {
+              "numero": 4,
+              "nombre": "INGENIERÍA CIVIL I",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": []
+            },
+            {
+              "numero": 5,
+              "nombre": "SISTEMAS DE REPRESENTACIÓN",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": []
+            },
+            {
+              "numero": 6,
+              "nombre": "FÍSICA I",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": []
+            },
+            {
+              "numero": 7,
+              "nombre": "QUÍMICA GENERAL",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": []
+            },
+            {
+              "numero": 8,
+              "nombre": "INGLÉS I",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": []
+            },
+            {
+              "numero": 9,
+              "nombre": "FUNDAMENTOS DE INFORMÁTICA",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": []
+            }
+          ]
+        },
+        {
+          "nivel": "II",
+          "asignaturas": [
+            {
+              "numero": 10,
+              "nombre": "ANÁLISIS MATEMÁTICO II",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": [1, 2]
+            },
+            {
+              "numero": 11,
+              "nombre": "ESTABILIDAD",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": [1, 2, 5, 6, 9]
+            },
+            {
+              "numero": 12,
+              "nombre": "INGENIERÍA CIVIL II",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": [3, 4, 5, 9]
+            },
+            {
+              "numero": 13,
+              "nombre": "TECNOLOGÍA DE LOS MATERIALES",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": [1, 5, 7, 6]
+            },
+            {
+              "numero": 14,
+              "nombre": "FÍSICA II",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": [1, 6]
+            },
+            {
+              "numero": 15,
+              "nombre": "PROBABILIDAD Y ESTADÍSTICA",
+              "para_cursar_aprobar": [],
+              "para_cursar_cursar": [1, 2]
+            }
+          ]
+        },
+        {
+          "nivel": "III",
+          "asignaturas": [
+            {
+              "numero": 16,
+              "nombre": "RESISTENCIA DE LOS MATERIALES",
+              "para_cursar_aprobar": [1, 2, 6, 9],
+              "para_cursar_cursar": [11]
+            },
+            {
+              "numero": 17,
+              "nombre": "TECNOLOGÍA DEL HORMIGÓN",
+              "para_cursar_aprobar": [1, 2, 7, 6],
+              "para_cursar_cursar": [13, 15, 8]
+            },
+            {
+              "numero": 18,
+              "nombre": "TECNOLOGÍA DE LA CONSTRUCCIÓN",
+              "para_cursar_aprobar": [1, 2, 4, 5, 7, 6, 9],
+              "para_cursar_cursar": [11, 12, 13, 8]
+            },
+            {
+              "numero": 19,
+              "nombre": "INSTALACIONES TERMOMECÁNICAS",
+              "para_cursar_aprobar": [1, 2, 4, 5, 7, 6],
+              "para_cursar_cursar": [12, 13, 14]
+            },
+            {
+              "numero": 20,
+              "nombre": "GEOTOPOGRAFÍA",
+              "para_cursar_aprobar": [1, 2, 4, 5, 6],
+              "para_cursar_cursar": [10, 12, 14, 15]
+            },
+            {
+              "numero": 21,
+              "nombre": "HIDRÁULICA GENERAL Y APLICADA",
+              "para_cursar_aprobar": [1, 2, 5, 6, 9],
+              "para_cursar_cursar": [10, 11, 12, 14, 15]
+            },
+            {
+              "numero": 22,
+              "nombre": "CÁLCULO AVANZADO",
+              "para_cursar_aprobar": [1, 2, 5, 6, 9],
+              "para_cursar_cursar": [10, 11, 13, 15]
+            },
+            {
+              "numero": 23,
+              "nombre": "INSTALACIONES ELÉCTRICAS Y ACÚSTICAS",
+              "para_cursar_aprobar": [1, 2, 4, 5, 7, 6],
+              "para_cursar_cursar": [12, 13, 14]
+            },
+            {
+              "numero": 24,
+              "nombre": "ECONOMÍA",
+              "para_cursar_aprobar": [1, 2, 3, 4, 9],
+              "para_cursar_cursar": [12, 15, 8]
+            },
+            {
+              "numero": 25,
+              "nombre": "INGLÉS TÉCNICO II",
+              "para_cursar_aprobar": [3, 4],
+              "para_cursar_cursar": [8]
+            }
+          ]
+        },
+        {
+          "nivel": "IV",
+          "asignaturas": [
+            {
+              "numero": 26,
+              "nombre": "GEOTECNIA",
+              "para_cursar_aprobar": [10, 11, 12, 13, 14, 15],
+              "para_cursar_cursar": [16, 17, 18, 20, 21]
+            },
+            {
+              "numero": 27,
+              "nombre": "INSTALACIONES SANITARIAS Y DE GAS",
+              "para_cursar_aprobar": [5, 7, 6, 9, 13],
+              "para_cursar_cursar": [18, 20, 21, 24]
+            },
+            {
+              "numero": 28,
+              "nombre": "ANÁLISIS ESTRUCTURAL I",
+              "para_cursar_aprobar": [10, 12, 11, 15],
+              "para_cursar_cursar": [16, 17]
+            },
+            {
+              "numero": 29,
+              "nombre": "DISEÑO ARQ. PLANEAMIENTO Y URBANISMO I",
+              "para_cursar_aprobar": [11, 12, 13, 8],
+              "para_cursar_cursar": [18, 20, 23, 19, 24, 25]
+            },
+            {
+              "numero": 30,
+              "nombre": "ESTRUCTURAS DE HORMIGÓN",
+              "para_cursar_aprobar": [10, 11, 12, 13, 14, 15],
+              "para_cursar_cursar": [16, 17, 18, 20, 25]
+            },
+            {
+              "numero": 31,
+              "nombre": "HIDROLOGÍA Y OBRAS HIDRÁULICAS",
+              "para_cursar_aprobar": [10, 11, 12, 13, 14, 15],
+              "para_cursar_cursar": [16, 18, 20, 21, 24, 25]
+            },
+            {
+              "numero": 32,
+              "nombre": "INGENIERÍA LEGAL",
+              "para_cursar_aprobar": [1, 2, 3, 4, 9],
+              "para_cursar_cursar": [10, 12, 15]
+            }
+          ]
+        },
+        {
+          "nivel": "V",
+          "asignaturas": [
+            {
+              "numero": 33,
+              "nombre": "CONSTRUCCIONES METÁLICAS Y DE MADERA",
+              "para_cursar_aprobar": [16, 17, 18, 20],
+              "para_cursar_cursar": [22, 28]
+            },
+            {
+              "numero": 34,
+              "nombre": "CIMENTACIONES",
+              "para_cursar_aprobar": [16, 17, 18, 20, 21],
+              "para_cursar_cursar": [22, 26, 28, 30, 31]
+            },
+            {
+              "numero": 35,
+              "nombre": "ORGANIZACIÓN Y CONDUCCIÓN DE OBRAS",
+              "para_cursar_aprobar": [17, 18, 20, 21, 23, 19, 24, 25],
+              "para_cursar_cursar": [26, 27, 29, 30, 31]
+            },
+            {
+              "numero": 36,
+              "nombre": "VIAS DE COMUNICACIÓN I",
+              "para_cursar_aprobar": [10, 11, 12, 13, 15, 8],
+              "para_cursar_cursar": [17, 18, 20]
+            },
+            {
+              "numero": 37,
+              "nombre": "ANÁLISIS ESTRUCTURAL II",
+              "para_cursar_aprobar": [16, 17, 18, 20, 25],
+              "para_cursar_cursar": [22, 26, 28, 30, 31]
+            },
+            {
+              "numero": 38,
+              "nombre": "INGENIERÍA SANITARIA",
+              "para_cursar_aprobar": [17, 18, 20, 21, 25],
+              "para_cursar_cursar": [26, 27, 31]
+            },
+            {
+              "numero": 39,
+              "nombre": "VIAS DE COMUNICACIÓN II",
+              "para_cursar_aprobar": [16, 17, 18, 20, 21, 24],
+              "para_cursar_cursar": [26, 30, 31, 32, 36]
+            },
+            {
+              "numero": 40,
+              "nombre": "GESTIÓN AMBIENTAL Y DESARROLLO SUSTENTABLE",
+              "para_cursar_aprobar": [21, 24, 25],
+              "para_cursar_cursar": [26, 29, 31, 32]
+            },
+            {
+              "numero": 41,
+              "nombre": "PROYECTO FINAL",
+              "para_cursar_aprobar": [8, 16, 17, 18, 20, 21, 24, 23, 19, 25],
+              "para_cursar_cursar": [26, 27, 32, 29, 28, 30, 31]
+            }
+          ]
+        }
+      ],
+      "condiciones_adicionales": {
+        "proyecto_final": "Ninguna condición adicional explícita para Proyecto Final más allá de las correlatividades de la asignatura.",
+        "practica_profesional_supervisada": "Ninguna condición adicional explícita para Práctica Profesional Supervisada."
+      }
+    }
+  
